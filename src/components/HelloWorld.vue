02d19566dd6c8c790b8ed9c570b89f6101a2c76c
Denis <denispolasek@gmail.com>
2022-11-07 00:07:38 +0100

    wip

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,7 @@
 <script setup lang="ts">
 
-import ClickableCounter from "./ClickableCounter.vue";
-import ClickableCounterUsingClass from "./ClickableCounterUsingClass.vue";
-import {ref} from "vue";
-
-const maxValue = ref(6);
-
-setTimeout(()=>{
-  maxValue.value = 5
-},2000)
-
 </script>
 
 <template>
-
-  <clickable-counter :initial-value="5" :max-value="maxValue"></clickable-counter>
-  <div></div>
-  <clickable-counter-using-class :initial-value="5" :max-value="maxValue"></clickable-counter-using-class>
 
 </template>
